32d3d11c2fc19c9fc10d9f589e2c7b8fa6d9edfa
Anthony <blacknebula236@gmail.com>
2025-04-15 21:26:50 +0930

    custom handlers + readiness handler

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 	const port = "8080"
+	const fileRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot))))
+	mux.HandleFunc("/healthz", handleReadiness)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -18,4 +21,10 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handleReadiness(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(200)
+	io.WriteString(resp, "OK")
 }
