fd0ec717e4dfe1282847f6dbf14d7f6665b045f6
Anthony <blacknebula236@gmail.com>
2025-04-16 16:21:49 +0930

    add metric middleware

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 	const fileRoot = "."
 
+	cfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot)))))
 	mux.HandleFunc("/healthz", handleReadiness)
+	mux.HandleFunc("/metrics", cfg.handleNumberOfRequests)
+	mux.HandleFunc("/reset", cfg.handleReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -27,4 +37,25 @@ func handleReadiness(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	resp.WriteHeader(200)
 	io.WriteString(resp, "OK")
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleNumberOfRequests(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(200)
+	respStr := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	io.WriteString(resp, respStr)
+}
+
+func (cfg *apiConfig) handleReset(resp http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	resp.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(200)
+	io.WriteString(resp, "Hits Reset")
 }
