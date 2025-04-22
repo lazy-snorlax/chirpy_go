b19b89a3e7a4500fd6b9e811ff523d2e5fcfff77
Anthony <blacknebula236@gmail.com>
2025-04-22 13:41:57 +0930

    add column to users tbl for chirpyred + query to upgrade to chirpyred

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -15,8 +15,16 @@ DELETE FROM users;
 -- name: GetUserByEmail :one
 SELECT * FROM users WHERE email = $1;
 
+-- name: GetUserById :one
+SELECT * FROM users WHERE id = $1;
+
 -- name: UpdateUserEmailAndPassword :one
 UPDATE users
 SET email = $1, hashed_password = $2, updated_at = NOW()
 WHERE id = $3
+RETURNING *;
+
+-- name: UpgradeUserToChirpyRed :one
+UPDATE users SET is_chirpy_red = true, updated_at = NOW()
+WHERE id = $1
 RETURNING *;
