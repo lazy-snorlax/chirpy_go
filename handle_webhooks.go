8f0bda877f313a26c847e90c5cfb32f3bb3d604d
Anthony <blacknebula236@gmail.com>
2025-04-22 13:43:39 +0930

    create endpoint to upgrade to chirpyred using a webhook

---
+++ handle_webhooks.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) userChirpyRed(resp http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Event string `json:"event"`
+		Data  struct {
+			UserId uuid.UUID `json:"user_id"`
+		}
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(resp, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	if params.Event != "user.upgraded" {
+		resp.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	_, err = cfg.db.UpgradeUserToChirpyRed(req.Context(), params.Data.UserId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(resp, http.StatusNotFound, "Couldn't find user", err)
+			return
+		}
+		respondWithError(resp, http.StatusInternalServerError, "error updating user", err)
+		return
+	}
+	resp.WriteHeader(http.StatusNoContent)
+}
