a971b68595cd11dc57f46118e706e61ba40c6738
Anthony <blacknebula236@gmail.com>
2025-04-22 11:20:01 +0930

    move chirps CRUD funcs to one file

--- handle_chirps.go
+++ handle_chirps.go
@@ -13,6 +13,53 @@ import (
 	"github.com/google/uuid"
 )
 
+type Chirp struct {
+	Id        string `json:"id"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	Body      string `json:"body"`
+	UserId    string `json:"user_id"`
+}
+
+func (cfg *apiConfig) getChirp(resp http.ResponseWriter, req *http.Request) {
+	chirpID := req.PathValue("chirpID")
+	chirp, err := cfg.db.GetChirp(context.Background(), uuid.MustParse(chirpID))
+	if err != nil {
+		respondWithError(resp, http.StatusNotFound, "Couldn't get chirp", err)
+		return
+	}
+	dat := Chirp{
+		Id:        chirp.ID.String(),
+		CreatedAt: chirp.CreatedAt.String(),
+		UpdatedAt: chirp.UpdatedAt.String(),
+		Body:      chirp.Body,
+		UserId:    chirp.UserID.String(),
+	}
+	respondWithJSON(resp, http.StatusOK, dat)
+}
+
+func (cfg *apiConfig) getAllChirps(resp http.ResponseWriter, req *http.Request) {
+	allChirps, err := cfg.db.GetAllChirps(context.Background())
+	if err != nil {
+		respondWithError(resp, http.StatusInternalServerError, "Couldn't get chirps", err)
+		return
+	}
+
+	var chirps []Chirp
+	for _, chirp := range allChirps {
+		dat := Chirp{
+			Id:        chirp.ID.String(),
+			CreatedAt: chirp.CreatedAt.String(),
+			UpdatedAt: chirp.UpdatedAt.String(),
+			Body:      chirp.Body,
+			UserId:    chirp.UserID.String(),
+		}
+		chirps = append(chirps, dat)
+	}
+
+	respondWithJSON(resp, http.StatusOK, chirps)
+}
+
 func (cfg *apiConfig) createChirp(resp http.ResponseWriter, req *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
